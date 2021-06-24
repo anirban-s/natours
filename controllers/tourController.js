bb2d9603ac6b3170b1d3b96c9abddc7b794fc7fa
anirban-s <baban.chrome@gmail.com>
2021-06-24 21:16:31 +0530

    eslint and prettier plugins are added for formatting and linting

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -5,12 +5,10 @@ const tours = JSON.parse(
 );
 
 exports.checkID = (req, res, next, val) => {
-  console.log(`Tour id is ${val}`);
-
   if (req.params.id * 1 > tours.length) {
     return res.status(404).json({
       status: 'fail',
-      message: 'Invalid ID',
+      message: 'Invalid ID'
     });
   }
   next();
@@ -20,7 +18,7 @@ exports.checkBody = (req, res, next) => {
   if (!req.body.name || !req.body.price) {
     return res.status(400).json({
       status: 'fail',
-      message: 'Missing name or price',
+      message: 'Missing name or price'
     });
   }
 };
@@ -31,20 +29,20 @@ exports.getAllTours = (req, res) => {
     requestedAt: req.requestedTime,
     results: tours.length,
     data: {
-      tours: tours,
-    },
+      tours: tours
+    }
   });
 };
 
 exports.getTour = (req, res) => {
   const id = req.params.id * 1;
-  const tour = tours.find((el) => el.id === id);
+  const tour = tours.find(el => el.id === id);
 
   res.status(200).json({
     status: 'success',
     data: {
-      tour: tour,
-    },
+      tour: tour
+    }
   });
 };
 
@@ -57,12 +55,12 @@ exports.createTour = (req, res) => {
   fs.writeFile(
     `${__dirname}/../dev-data/data/tours-simple.json`,
     JSON.stringify(tours),
-    (err) => {
+    err => {
       res.status(201).json({
         status: 'success',
         data: {
-          tour: newTour,
-        },
+          tour: err
+        }
       });
     }
   );
@@ -72,14 +70,14 @@ exports.updateTour = (req, res) => {
   res.status(200).json({
     status: 'success',
     data: {
-      tour: '<Updated tour>....',
-    },
+      tour: '<Updated tour>....'
+    }
   });
 };
 
 exports.deleteTour = (req, res) => {
   res.status(204).json({
     status: 'success',
-    data: null,
+    data: null
   });
 };
