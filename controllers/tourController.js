dc468a87c440166616127174ed7e3255499b2985
anirban-s <baban.chrome@gmail.com>
2021-06-30 18:15:13 +0530

    sorting and advanced filter implemented

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -25,8 +25,33 @@ const Tour = require('./../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    // BUILD QUERY
+    // 1A) Filtering
+    const queryObj = { ...req.query };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach(el => delete queryObj[el]);
 
+    // 1B) Advanced filtering
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
+
+    let query = Tour.find(JSON.parse(queryStr));
+
+    // 2) Sorting
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ');
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort('-createdAt');
+    }
+
+    // EXECUTE QUERY
+    const tours = await query;
+
+    // { difficulty: 'easy', duration: { $gte: 5} }
+    // const query = Tour.find().where('duration').equals(5);
+
+    // SEND RESPONSE
     res.status(200).json({
       status: 'success',
       results: tours.length,
