68bbc6976cb053207977df28b579b36b9a7cc60a
anirban-s <baban.chrome@gmail.com>
2021-07-16 21:05:38 +0530

    prettierrc is updated with printWidth

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -26,6 +26,7 @@ const handleJWTExpiredError = () =>
   new AppError('Your token has expired! Please log in again.', 401);
 
 const sendErrorDev = (err, res) => {
+  console.log(err.statusCode);
   res.status(err.statusCode).json({
     status: err.status,
     error: err,
