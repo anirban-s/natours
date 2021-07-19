8e592f5ba5350bcfed3e43f9aa6f3507808bfb65
anirban-s <baban.chrome@gmail.com>
2021-07-19 19:14:20 +0530

    hpp packeg is added

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -30,12 +33,32 @@ app.use('/api', limiter);
 // Body Parser, reading data from body into req.body
 app.use(express.json({ limit: '10kb' }));
 
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
+
+// Prevent http parameter polution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'maxGroupSize',
+      'difficulty',
+      'price'
+    ]
+  })
+);
+
 // Service static files
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
   req.requestedTime = new Date().toISOString();
-  console.log(req.headers);
+  //console.log(req.headers);
 
   next();
 });
